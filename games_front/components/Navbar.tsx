6500f078ac049a2c60d9883d6947ffb0a78049af
Carlos Fiestas <cfiestas6@gmail.com>
2024-03-16 21:34:00 +0000

    alabama

--- games_front/components/Navbar.tsx
+++ games_front/components/Navbar.tsx
@@ -3,9 +3,45 @@ import { useAccount } from 'wagmi';
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '/static/logo.svg'
+import { celoAlfajores } from "viem/chains";
+import { useState, useEffect } from 'react';
+import { Chain } from '@rainbow-me/rainbowkit';
+import {
+    createPublicClient,
+    createWalletClient,
+    custom,
+    getContract,
+    http,
+    parseEther,
+    stringToHex,
+} from "viem";
+
+const cUSDTokenAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"; // Testnet
 
 export default function Navbar() {
     const { isConnected } = useAccount();
+    const [address, setAddress] = useState<string | null>(null);
+
+    const publicClient = createPublicClient({
+        chain: celoAlfajores as Chain,
+        transport: http(),
+    });
+
+    const getUserAddress = async () => {
+        if (typeof window !== "undefined" && window.ethereum) {
+            let walletClient = createWalletClient({
+                transport: custom(window.ethereum),
+                chain: celoAlfajores as Chain,
+            });
+
+            let [address] = await walletClient.getAddresses();
+            setAddress(address);
+        }
+    };
+
+    useEffect(() => {
+        getUserAddress();
+    }, []);
 
     return (
         <div className="navbar">
@@ -22,7 +58,7 @@ export default function Navbar() {
         </div>
         <div className="flex-none ml-5">
             { isConnected && <Link className='mr-10' href='/dashboard'>Dashboard</Link>}
-            <ConnectButton label="Sign In" />
+            {!window.ethereum.isMiniPay && <ConnectButton label="Sign In" />}
         </div>
         </div> 
     );
