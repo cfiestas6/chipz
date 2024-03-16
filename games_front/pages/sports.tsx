4379b4df279c1598d2b95503609bf69becbb5380
Carlos Fiestas <cfiestas6@gmail.com>
2024-03-16 08:12:33 +0000

    mic mic

---
+++ games_front/pages/sports.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Hero from '../components/Hero';
+
+const Sports: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <meta
+          content="chipz"
+          name="chipz"
+        />
+        <link href="/static/logo.svg" rel="icon" />
+        <title>chipz</title>
+      </Head>
+
+      <main className="container min-h-lvh mx-auto px-4">
+        <Hero title="Sport Active Pools" subtitle="Upcoming games" />
+      </main>
+    </div>
+  );
+};
+
+export default Sports;
