8a07e020d948465b3a204b692549d1154f67cc32
Carlos Fiestas <cfiestas6@gmail.com>
2024-03-17 00:19:48 +0000

    asdf

--- games_front/components/IndexHero.tsx
+++ games_front/components/IndexHero.tsx
@@ -6,8 +6,8 @@ export default function IndexHero(): any {
   <div className="hero-content flex-col lg:flex-row-reverse">
     <Image width={100} height={100} alt="a" src="" className="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p className="text-5xl font-bold">Create bet pools, ...</p>
+      <p className="py-6">what ever</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
